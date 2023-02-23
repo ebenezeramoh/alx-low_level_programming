7716ba1705915125624a95ed88a12b90b4c8f973
ebenezeramoh <ebenezeramoh35@gmail.com>
2023-02-23 13:43:31 -0800

    .

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * main - tests function that print if negativeis positive or negative
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+	i=98;
+	positive_or_negative(i);
+	return (0);
+}
